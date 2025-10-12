a33b1c99508e283bfb67003fab2b320c2d4ae1b0
Hemishahuja <122426053+Hemishahuja@users.noreply.github.com>
2025-10-12 16:03:37 -0400

    Delete src directory

--- src/main/java/com/example/stockeasy/repo/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains repository interfaces.
- * // TODO(repo): Define repository interfaces for data access.
- */
-package com.example.stockeasy.repo;
--- src/main/java/com/example/stockeasy/service/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains service interfaces.
- * // TODO(service): Define service interfaces for business logic.
- */
-package com.example.stockeasy.service;
