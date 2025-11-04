e6fd91dcfc0fa028053a7e7e8d086e6997d38e58
arushibisht09 <arushi.bisht09@gmail.com>
2025-11-03 21:02:04 -0500

    Update StockEasyApplication.java

--- src/main/java/com/example/stockeasy/StockEasyApplication.java
+++ src/main/java/com/example/stockeasy/StockEasyApplication.java
@@ -2,7 +2,7 @@ package com.example.stockeasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// UI runs locally while Spring Boot is running
 @SpringBootApplication
 public class StockEasyApplication {
 
